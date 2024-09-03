03900a2b7fef40b2edb155eaedbf4f727e0fa519
fredericodgv <fredericodgv@gmail.com>
2024-09-03 17:33:13 -0300

    Faz função de criar e matar processos

--- proc.c
+++ proc.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <ctype.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
 
 void list_processes()
 {
@@ -53,10 +58,66 @@ void list_processes()
     printf("\nTotal de processos listados: %d\n\n", process_count);
 }
 
+
+void kill_process(const char *pid_str, int force)
+{
+    char *endptr;
+    pid_t pid_num = strtol(pid_str, &endptr, 10);
+
+    if (*endptr != '\0' || pid_num <= 0)
+    {
+        fprintf(stderr, "PID inválido: %s\n", pid_str);
+        return;
+    }
+
+    int signal = force ? 9 : 15; // SIGKILL = 9, SIGTERM = 15
+
+    // Chamada de sistema para enviar sinal
+    long result = syscall(__NR_kill, pid_num, signal);
+
+    if (result == -1)
+    {
+        perror("Erro ao enviar sinal para o processo");
+    }
+    else
+    {
+        printf("Sinal %s enviado para o processo %d\n", force ? "SIGKILL" : "SIGTERM", pid_num);
+    }
+}
+
+
+void create_process()
+{
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("Erro ao criar processo");
+    }
+    else if (pid == 0)
+    {
+        // Código do processo filho
+        printf("Processo filho criado com PID %d, PID do pai é %d\n", getpid(), getppid());
+        while (1)
+        {
+            sleep(1);
+        }
+        
+    }
+    else
+    {
+        // Código do processo pai
+        printf("Processo pai criou o processo filho com PID %d, PID do pai é %d\n", pid, getpid());
+    }
+}
+
 int main()
 {
     printf("Comandos disponíveis:\n");
     printf(" - list : Lista todos os processos em execução.\n");
+    printf(" - create : Cria um novo processo.\n");
+    printf(" - kill <pid> : Envia um sinal SIGTERM para o processo com o PID especificado.\n");
+    printf(" - kill --force <pid> : Envia um sinal SIGKILL para o processo com o PID especificado.\n");
     printf(" - exit : Sai do programa.\n");
     printf("\n");
 
@@ -64,15 +125,28 @@ int main()
 
     while (1)
     {
-        printf("Digite um comando: ");
+        printf("PROC %d: Digite um comando: ", getpid());
         if (fgets(command, sizeof(command), stdin) != NULL)
         {
             // Remove newline character
             command[strcspn(command, "\n")] = '\0';
 
-            if (strcmp(command, "list") == 0)
+            if (strncmp(command, "list", 4) == 0)
             {
                 list_processes();
+            }
+            else if (strncmp(command, "create", 6) == 0)
+            {
+                create_process();
+                sleep(1);
+            }
+            else if (strncmp(command, "kill --force ", 13) == 0)
+            {
+                kill_process(command + 13, 1);
+            }
+            else if (strncmp(command, "kill ", 5) == 0)
+            {
+                kill_process(command + 5, 0);
             }
             else if (strcmp(command, "exit") == 0)
             {
